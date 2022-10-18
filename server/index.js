8fc76ad78c776171fde0d63e91bf289912c183b9
aaronliuo <aaron.j.liu@gmail.com>
2022-10-18 16:30:49 -0400

    Moved MongoDB URL

--- server/index.js
+++ server/index.js
@@ -2,19 +2,21 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
 
+dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://lulusushi:LmCcSumYXUqpRrTI@cluster0.vxyaaii.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(
   () => app.listen(PORT, () => console.log(`Server running on ${PORT}`))).catch(
